8f206636c1060b78bd6419979ab6bdd67e9b254b
Abid Shahrier <abidofficial0001@gmail.com>
2024-01-11 04:39:37 +0600

    Added initial files

---
+++ README.md
@@ -0,0 +1,7 @@
+# CSS Animation
+
+This page is about CSS animation
+
+This repository has 2 CSS files. Try each file. Both are not same.
+
+Author - Abid Shahrier
---
+++ script.js
@@ -0,0 +1,12 @@
+let box = document.querySelector(".box");
+
+let turn = 0;
+box.addEventListener("click", () => {
+    if(turn === 0) {
+        turn = 1;
+        box.style.animationPlayState = "running";
+    }else{
+        turn = 0;
+        box.style.animationPlayState = "paused";
+    }
+});
