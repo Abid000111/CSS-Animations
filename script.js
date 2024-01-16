6ca4f369592710fe53d339d511d52b623b3b617b
Abid Shahrier <abidofficial0001@gmail.com>
2024-01-16 15:38:20 +0600

    Updated HTML, CSS, JS

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ let box = document.querySelector(".box");
 
 let turn = 0;
 box.addEventListener("click", () => {
-    if(turn === 0) {
-        turn = 1;
-        box.style.animationPlayState = "running";
-    }else{
-        turn = 0;
-        box.style.animationPlayState = "paused";
-    }
+	if (turn === 0) {
+		turn = 1;
+		box.style.animationPlayState = "running";
+	} else {
+		turn = 0;
+		box.style.animationPlayState = "paused";
+	}
 });
